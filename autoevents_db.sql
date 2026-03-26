117c2e1afe2207145879ba5e3b0c14a46d5823ac
C4brerx <acabreracaceres@gmail.com>
2026-03-26 16:46:38 +0100

    Integración completa de IA real con Gemini (Diagnóstico interactivo)

--- autoevents_db.sql
+++ autoevents_db.sql
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla autoevents_db.usuarios: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla autoevents_db.usuarios: ~2 rows (aproximadamente)
 INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `fecha_registro`) VALUES
 	(3, 'Adrian Cabrera Caceres', 'acabreracaceres@gmail.com', '$2y$10$H7fyOoQnowqfxd8NULj4tuTvm.uP7PtN1DJ/68clmhWKzFtWdiDE.', '2026-03-19 13:21:02'),
 	(4, 'Pruebas', 'P@gmail.es', '$2y$10$Xqf1S3MIhP4xVM/cvj5FMuS3VqX71WEyBS.eGEctrY8pgG2PVP94O', '2026-03-19 16:00:20');
@@ -78,12 +78,13 @@ CREATE TABLE IF NOT EXISTS `vehiculos` (
   PRIMARY KEY (`id`),
   KEY `usuario_id` (`usuario_id`),
   CONSTRAINT `vehiculos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla autoevents_db.vehiculos: ~2 rows (aproximadamente)
 INSERT INTO `vehiculos` (`id`, `usuario_id`, `marca`, `modelo`, `anio`, `motor`, `especificaciones`, `modificaciones`, `fotos`) VALUES
 	(4, 3, 'BMW', 'M4', 2022, 'BMW M4 Competition (Coupé / Convertible) Potencia:', NULL, NULL, '1773935910-9978-2022-bmw-m4-csl-15.jpg'),
-	(5, 4, 'BMW', 'M4 Competicione', 2022, '3.0L S58', ' motor gasolina 3.0L TwinPower Turbo de 6 cilindros en línea, con potencias de 480 CV (base) a 510 CV (Competition) y hasta 650 Nm de par, asociado a tracción trasera o M xDrive y cambio automático de 8 velocidades. Es conocido por su aceleración de 0 a 100 km/h en 3,9 segundos', NULL, '1773937634-9008-2022-bmw-m4-csl.webp,1773937634-7149-2022-bmw-m4-csl-15.jpg');
+	(5, 4, 'BMW', 'M4 Competition', 2022, '3.0L S58', ' motor gasolina 3.0L TwinPower Turbo de 6 cilindros en línea, con potencias de 480 CV (base) a 510 CV (Competition) y hasta 650 Nm de par, asociado a tracción trasera o M xDrive y cambio automático de 8 velocidades. Es conocido por su aceleración de 0 a 100 km/h en 3,9 segundos', NULL, '1773937634-9008-2022-bmw-m4-csl.webp,1773937634-7149-2022-bmw-m4-csl-15.jpg'),
+	(7, 4, 'DS', 'DS7', 2021, '1.6', '300cv', NULL, '1774533985-9130-article-ds-7-crossback-suv-precios-gama-espana-59e741366e01f.jpeg');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
